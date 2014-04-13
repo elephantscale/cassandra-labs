191df4940e9d29d87b3913176c87cbcacd00155b
Sujee Maniyam (mac2) <sujee@sujee.net>
2014-04-12 23:48:02 -0700

    test connector

---
+++ client-java/src/main/java/com/es/cassandra/TestConnect.java
@@ -0,0 +1,23 @@
+package com.es.cassandra;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.Session;
+
+public class TestConnect {
+
+  public static void main(String[] args) throws Exception {
+
+    Cluster cluster = Cluster.builder().addContactPoint("localhost").build();
+    System.out.println("connected to " + cluster.getClusterName());
+
+    Session session = cluster.connect("myflix");
+    System.out.println("session initialized to " + session.getLoggedKeyspace());
+
+
+    session.close();
+    cluster.close();
+
+  }
+
+}
