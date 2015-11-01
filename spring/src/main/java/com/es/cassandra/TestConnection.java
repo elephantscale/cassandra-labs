f3cf282d38ee314b846c40becd71a60042776521
Mark Kerzner <mark@elephantscale.com>
2015-11-01 09:57:29 -0500

    start spring project as a copy from java-driver

---
+++ spring/src/main/java/com/es/cassandra/TestConnection.java
@@ -0,0 +1,28 @@
+package com.es.cassandra;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
+// Javadocs : http://www.datastax.com/drivers/java/2.0/index.html
+
+public class TestConnection {
+
+  public static void main(String[] args) throws Exception {
+
+    Cluster cluster = Cluster.builder().addContactPoint("localhost").build();
+    System.out.println("connected to " + cluster.getClusterName());
+
+    // TODO : create a session by connecting to cluster
+    // Session  session = cluster.connect ("keyspace_name");
+
+    // TODO : print out some session details
+    // System.out.println("session initialized to " + session.getLoggedKeyspace());
+
+    // TODO : close the session
+    // session.close();
+
+    cluster.close();
+
+  }
+
+}
